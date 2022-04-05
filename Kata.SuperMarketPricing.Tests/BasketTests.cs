a5d2045a3022da3e07133e75c6834eab7a75ab48
Cyrus <cyrus.penuliar@gmail.com>
2022-04-05 23:45:52 +0800

    Buy 2 get 1 free price scheme

--- Kata.SuperMarketPricing.Tests/BasketTests.cs
+++ Kata.SuperMarketPricing.Tests/BasketTests.cs
@@ -9,6 +9,22 @@ namespace Kata.SuperMarketPricing.Tests
 {
     public class BasketTests
     {
+        public void Should_compute_products_with_simple_price_scheme()
+        {
+            // Arrange
+            var product = new Product(Guid.NewGuid(), "Test Product", 2, UnitOfMeasure.pc, new SimplePriceScheme());
+
+            var basket = Basket.Empty;
+
+            var basketItem = new BasketItem(product, 2, UnitOfMeasure.pc);
+
+            // Act
+            var result = basket.AddItem(basketItem);
+
+            // Assert
+            result.Total.Should().Be(2);
+        }
+
         [Theory]
         [InlineData(8, 6)]
         [InlineData(2, 2)]
@@ -17,7 +33,7 @@ namespace Kata.SuperMarketPricing.Tests
             double totalPrice)
         {
             // Arrange
-            var product = new Product(Guid.NewGuid(), "Bundled Product", 1, UnitOfMeasure.pc, new BundledProductPriceScheme());
+            var product = new Product(Guid.NewGuid(), "Test Product", 1, UnitOfMeasure.pc, new BundledProductPriceScheme());
 
             var basket = Basket.Empty;
 
@@ -34,14 +50,36 @@ namespace Kata.SuperMarketPricing.Tests
         [InlineData(2, UnitOfMeasure.Kg, 8.77)]
         [InlineData(20, UnitOfMeasure.oz, 2.49)]
         [InlineData(10, UnitOfMeasure.lb, 19.9)]
-        public void Should_compute_converted_price_scheme(double quantity, UnitOfMeasure uom, double totalPrice)
+        public void Should_compute_products_with_converted_price_scheme(double quantity, UnitOfMeasure uom, double totalPrice)
         {
             // Arrange
-            var product = new Product(Guid.NewGuid(), "Bundled Product", 1.99, UnitOfMeasure.lb, new ConvertedPriceScheme());
+            var product = new Product(Guid.NewGuid(), "Test Product", 1.99, UnitOfMeasure.lb, new ConvertedPriceScheme());
 
             var basket = Basket.Empty;
 
             var basketItem = new BasketItem(product, quantity, uom);
+
+            // Act
+            var result = basket.AddItem(basketItem);
+
+            // Assert
+            result.Total.Should().Be(totalPrice);
+        }
+
+        [Theory]
+        [InlineData(3, 1)]
+        [InlineData(6, 2)]
+        [InlineData(8, 3)]
+        public void Should_compute_buy_2_get_1_free_products_price_scheme(
+            double quantity,
+            double totalPrice)
+        {
+            // Arrange
+            var product = new Product(Guid.NewGuid(), "Test Product", 1, UnitOfMeasure.pc, new BuyAndGetFreeItemsPriceScheme());
+
+            var basket = Basket.Empty;
+
+            var basketItem = new BasketItem(product, quantity, UnitOfMeasure.pc);
 
             // Act
             var result = basket.AddItem(basketItem);
---
+++ Kata.SupermarketPricing/Models/PriceSchemes/BuyAndGetFreeItemsPriceScheme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Kata.SupermarketPricing.Models.PriceSchemes
+{
+    public sealed class BuyAndGetFreeItemsPriceScheme : IPriceScheme
+    {
+        private const int PricedItemCount = 2;
+
+        private const int FreeItemCount = 1;
+
+        public double ComputeItemPrice(Product product, ItemMeasure itemMeasure)
+        {
+            var quantity = itemMeasure.Quantity;
+
+            if (quantity < PricedItemCount)
+                throw new ArgumentException("Items have insufficient count for the price scheme to apply");
+
+            var bundledQuantity = Math.Ceiling(quantity / (PricedItemCount + FreeItemCount));
+
+            return product.Price * bundledQuantity;
+        }
+    }
+}
